1080918eef0a91c5e0a5b2304e2b8e2589191976
chenyu <1084966084@qq.com>
2021-06-04 07:26:10 +0800

    2021.6.4 add constant_info constant_pool class_file

---
+++ ch03/classfile/class_file.go
@@ -0,0 +1,5 @@
+package classfile
+
+type ClassFile struct {
+	
+}
---
+++ ch03/classfile/constant_info.go
@@ -0,0 +1,9 @@
+package classfile
+
+const (
+	CONSTANT_Class					= 7
+	CONSTANT_FieldRef				= 9
+	CONSTANT_MethodRef				= 10
+	CONSTANT_InterfaceMethodRef		= 11
+	CONSTANT_String					= 8
+)
