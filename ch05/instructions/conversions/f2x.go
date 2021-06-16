20f788cedf10b10c45d73ece3e1e2ffb2a29240e
chenyu <1084966084@qq.com>
2021-06-16 11:26:14 +0800

    2021.6.16 complete math and conversions instructions

---
+++ ch05/instructions/conversions/f2x.go
@@ -0,0 +1,29 @@
+package conversions
+
+import (
+	"go-jvm/ch05/instructions/base"
+	"go-jvm/ch05/rtda"
+)
+
+//float型数据转换
+type F2I struct { base.NoOperandsInstruction }
+type F2L struct { base.NoOperandsInstruction }
+type F2D struct { base.NoOperandsInstruction }
+
+func (self *F2I) Execute(frame *rtda.Frame)  {
+	stack := frame.OperandStack()
+	val := stack.PopFloat()
+	stack.PushInt(int32(val))
+}
+
+func (self *F2L) Execute(frame *rtda.Frame)  {
+	stack := frame.OperandStack()
+	val := stack.PopFloat()
+	stack.PushLong(int64(val))
+}
+
+func (self *F2D) Execute(frame *rtda.Frame)  {
+	stack := frame.OperandStack()
+	val := stack.PopFloat()
+	stack.PushDouble(float64(val))
+}
