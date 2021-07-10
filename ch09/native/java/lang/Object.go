2a0f670a03c773372eae10494e2b171e2423ba5f
chenyu <1084966084@qq.com>
2021-07-10 15:08:52 +0800

    feat(ch09): add Object.hashCode native method

--- ch09/native/java/lang/Object.go
+++ ch09/native/java/lang/Object.go
@@ -3,10 +3,19 @@ package lang
 import (
 	"go-jvm/ch09/native"
 	"go-jvm/ch09/rtda"
+	"unsafe"
 )
 
 func init() {
 	native.Register("java/lang/Object", "getClass", "()Ljava/lang/Class;", getClass)
+	//public native int hashCode();
+	native.Register("java/lang/Object", "hashCode", "()I", hashCode)
+}
+
+func hashCode(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	hash := int32(uintptr(unsafe.Pointer(this)))
+	frame.OperandStack().PushInt(hash)
 }
 
 func getClass(frame *rtda.Frame) {
